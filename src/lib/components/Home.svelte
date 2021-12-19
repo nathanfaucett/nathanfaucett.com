c597be4c08381ff86575dc7a1b2a37083ecb6dec
Nathan Faucett <nathanfaucett@gmail.com>
2021-12-18 22:12:32 -0500

    blur it

---
+++ src/lib/components/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<div
+	class="bg-fixed bg-cover bg-center flex flex-grow"
+	style="background-image: url(/background.jpg)"
+>
+	<div class="flex flex-grow flex-col backdrop-blur-lg">
+		<section class="m-auto">
+			<h1 class="name relative text-blue-100 p-2 text-9xl" style="color: #00ffff;">
+				<span>Nathan Faucett</span>
+				<span class="absolute mix-blend-multiply left-1 top-1" style="color: #ff0000;"
+					>Nathan Faucett</span
+				>
+			</h1>
+		</section>
+
+		<Footer />
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,9 @@
 <script>
-	import Footer from '$lib/Footer.svelte';
+	import Home from '$lib/components/Home.svelte';
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<div
-	class="bg-fixed bg-cover bg-center flex flex-grow flex-col"
-	style="background-image: url(/background.jpg)"
->
-	<section class="m-auto">
-		<h1 class="name relative text-blue-100 p-2 text-9xl" style="color: #00ffff;">
-			<span>Nathan Faucett</span>
-			<span class="absolute mix-blend-multiply left-1 top-1" style="color: #ff0000;"
-				>Nathan Faucett</span
-			>
-		</h1>
-	</section>
-
-	<Footer />
-</div>
+<Home />
